1f58424de7a9b5d246acb371848fc31f5dcc49bc
Pablo Melendez <pablofer.melendez@gmail.com>
2018-10-15 11:07:15 -0500

    login try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Body } from './presentational/Box';
+import { Input, Label, LogInBox } from './presentational/Form';
+import { Avatar } from './presentational/Image';
+import { ATag, H1 } from './presentational/Text';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Body>
+        <LogInBox>
+        <H1>Log In</H1>
+          <form>
+            <Label htmlFor='username'>Username</Label>
+            <Input type="text" placeholder="Username" />
+
+            <Label htmlFor='username'>Password</Label>
+            <Input type="text" placeholder="Password" />
+
+            <Input type="submit" value="Log In" />
+            <ATag href='#'>Forgot Password</ATag>
+            <br />
+            <ATag href='#'>Don't have an account</ATag>
+          </form>
+        </LogInBox>
+      </Body>
     );
   }
 }
